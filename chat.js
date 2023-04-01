6fe4e3233bcdc474438e2e9efeb8db19efafc16c
Pisit Wongsripisant <act21078@gmail.com>
2023-04-01 21:24:26 +0700

    init  random name when connect / setUsername with duplicate name check

--- chat.js
+++ chat.js
@@ -1,7 +1,7 @@
 const uuidv4 = require("uuid").v4;
 
 const messages = new Set();
-const users = new Map();
+const users = new Map(); // userSocket => userName
 
 const defaultUser = {
   id: "anon",
@@ -10,13 +10,27 @@ const defaultUser = {
 
 const messageExpirationTimeMS = 5 * 60 * 1000;
 
+const initName = () => {
+  return "Anonymous" + uuidv4().slice(0, 5);
+};
+
 class Connection {
   constructor(io, socket) {
     this.socket = socket;
     this.io = io;
 
+    // add new user with random name
+    users.set(socket, initName());
+    console.log(
+      `User connected socket id: ${socket.id} with initial name: ${users.get(
+        socket
+      )}`
+    );
+    console.log(`There are ${users.size} users now\n.`);
+
     socket.on("getMessages", () => this.getMessages());
     socket.on("message", (value) => this.handleMessage(value));
+    socket.on("setUsername", (userName) => this.setUsername(userName));
     socket.on("disconnect", () => this.disconnect());
     socket.on("connect_error", (err) => {
       console.log(`connect_error due to ${err.message}`);
@@ -46,6 +60,22 @@ class Connection {
       messages.delete(message);
       this.io.sockets.emit("deleteMessage", message.id);
     }, messageExpirationTimeMS);
+  }
+
+  checkDuplicateName(userName) {
+    const allUserName = Array.from(users.values());
+    if (allUserName.includes(userName)) {
+      return true;
+    }
+    return false;
+  }
+
+  setUsername(userName) {
+    if (this.checkDuplicateName(userName)) {
+      this.io.sockets.emit("errorDuplicateName", userName);
+    } else {
+      users.set(this.socket, userName);
+    }
   }
 
   disconnect() {
