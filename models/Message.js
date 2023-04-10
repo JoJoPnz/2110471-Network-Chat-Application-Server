1fa92fd4e7da500da81874f0971730329ba84e32
Pisit Wongsripisant <act21078@gmail.com>
2023-04-10 17:02:23 +0700

    fix message model

--- models/Message.js
+++ models/Message.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserType = Object.freeze({
-  User: 'User',
-  System: 'System'
+  User: "User",
+  System: "System",
 });
 
 const MessageSchema = new mongoose.Schema({
@@ -12,9 +12,22 @@ const MessageSchema = new mongoose.Schema({
     required: true,
   },
   sender: {
-    type: [mongoose.Schema.Types.ObjectId],
+    type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    default: null,
+    required: function () {
+      return this.type === UserType.User;
+    },
+    // ensure that the field value is null when the type field is System,
+    // even if the sender field value is set.
+    validate: {
+      validator: function (v) {
+        return this.type === UserType.System ? v === null : true;
+      },
+      message: "Sender must be null when type is System",
+    },
+    default: function () {
+      return this.type === UserType.System ? null : undefined;
+    },
   },
   text: {
     type: String,
