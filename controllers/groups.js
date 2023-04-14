6f4045f41cf2e7c76c1027127c5a169164833282
Pisit Wongsripisant <act21078@gmail.com>
2023-04-14 21:15:11 +0700

    fix: join, leave, createMessage for group route

--- controllers/groups.js
+++ controllers/groups.js
@@ -97,15 +97,14 @@ exports.createGroup = async (req, res, next) => {
   }
 };
 
-//@desc     Update group (add user, add message, change name)
-//          request body: users: string[], removeUsers: string[],
-//                        messages: string[], name: string
-//@route    PATCH /api/v1/groups/:id
+//@desc     Join group
+//@route    POST /api/v1/groups/join
 //@access   Private
-exports.updateGroup = async (req, res, next) => {
+exports.joinGroup = async (req, res, next) => {
   try {
-    const groupId = req.params.id;
-    const { name, users, messages, removeUsers } = req.body;
+    const user = req.user;
+    const userId = req.user.id;
+    const groupId = req.body.groupId;
     const group = await Group.findById(groupId);
 
     if (!group) {
@@ -115,48 +114,162 @@ exports.updateGroup = async (req, res, next) => {
       });
     }
 
-    if (name) {
-      group.name = name;
-    }
+    // add group to user
+    await User.updateOne({ _id: userId }, { $addToSet: { groups: groupId } });
+    // user.groups.addToSet(groupId);
+    // await user.save();
 
-    if (users) {
-      group.users.addToSet(...users);
-      // update the groups field of all users in the new users array
-      await User.updateMany(
-        { _id: { $in: users } },
-        { $addToSet: { groups: group._id } }
-      );
-    }
-
-    if (messages) {
-      group.messages.push(...messages);
-    }
-
-    if (removeUsers) {
-      const updatedUsers = group.users.filter(
-        (userId) => !removeUsers.includes(String(userId))
-      );
-      group.users = updatedUsers;
-      // update the groups field of all removed users
-      await User.updateMany(
-        { _id: { $in: removeUsers } },
-        { $pull: { groups: group._id } }
-      );
-    }
-
+    // add user to group
+    group.users.addToSet(userId);
     await group.save();
+
     return res.status(200).json({ success: true, data: group });
   } catch (err) {
     console.log(err.stack);
-    // duplicate group name
-    if (err.code && err.code === 11000) {
-      return res
-        .status(400)
-        .json({ success: false, message: "This group name has already taken" });
-    }
     return res.status(500).json({
       success: false,
-      message: `Cannot update Group`,
+      message: `Cannot join Group'`,
     });
   }
 };
+
+//@desc     Leave group
+//@route    POST /api/v1/groups/leave
+//@access   Private
+exports.leaveGroup = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const userId = req.user.id;
+    const groupId = req.body.groupId;
+    const group = await Group.findById(groupId);
+
+    if (!group) {
+      return res.status(404).json({
+        success: false,
+        message: `No Group with the id of ${groupId}`,
+      });
+    }
+
+    // delete group from user
+    await User.updateOne({ _id: userId }, { $pull: { groups: groupId } });
+    // user.groups = user.groups.filter(
+    //   (id) => id.toString() !== groupId.toString()
+    // );
+    // await user.save();
+
+    // delete user from group
+    group.users = group.users.filter(
+      (id) => id.toString() !== userId.toString()
+    );
+    await group.save();
+
+    return res.status(200).json({ success: true, data: group });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(500).json({
+      success: false,
+      message: `Cannot leave Group'`,
+    });
+  }
+};
+
+//@desc     Add message
+//@route    POST /api/v1/groups/message
+//@access   Private
+exports.addMessageGroup = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const userId = req.user.id;
+    const groupId = req.body.groupId;
+    var tmpMessage = req.body.message;
+    // add sender to message
+    const message = Object.assign({}, tmpMessage, { sender: userId });
+    const group = await Group.findById(groupId);
+
+    if (!group) {
+      return res.status(404).json({
+        success: false,
+        message: `No Group with the id of ${groupId}`,
+      });
+    }
+
+    if (!group.users.includes(userId)) {
+      return res.status(403).json({ message: "User not in group" });
+    }
+
+    group.messages.push(message);
+    await group.save();
+
+    return res.status(200).json({ success: true, data: group });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(500).json({
+      success: false,
+      message: `Cannot add Message to Group`,
+    });
+  }
+};
+
+//@desc     Update group (add user, add message, change name)
+//          request body: users: string[], removeUsers: string[],
+//                        messages: string[], name: string
+//@route    PATCH /api/v1/groups/:id
+//@access   Private
+// exports.updateGroup = async (req, res, next) => {
+//   try {
+//     const groupId = req.params.id;
+//     const { name, users, messages, removeUsers } = req.body;
+//     const group = await Group.findById(groupId);
+
+//     if (!group) {
+//       return res.status(404).json({
+//         success: false,
+//         message: `No Group with the id of ${groupId}`,
+//       });
+//     }
+
+//     if (name) {
+//       group.name = name;
+//     }
+
+//     if (users) {
+//       group.users.addToSet(...users);
+//       // update the groups field of all users in the new users array
+//       await User.updateMany(
+//         { _id: { $in: users } },
+//         { $addToSet: { groups: group._id } }
+//       );
+//     }
+
+//     if (messages) {
+//       group.messages.push(...messages);
+//     }
+
+//     if (removeUsers) {
+//       const updatedUsers = group.users.filter(
+//         (userId) => !removeUsers.includes(String(userId))
+//       );
+//       group.users = updatedUsers;
+//       // update the groups field of all removed users
+//       await User.updateMany(
+//         { _id: { $in: removeUsers } },
+//         { $pull: { groups: group._id } }
+//       );
+//     }
+
+//     await group.save();
+//     return res.status(200).json({ success: true, data: group });
+//   } catch (err) {
+//     console.log(err.stack);
+//     // duplicate group name
+//     if (err.code && err.code === 11000) {
+//       return res
+//         .status(400)
+//         .json({ success: false, message: "This group name has already taken" });
+//     }
+//     return res.status(500).json({
+//       success: false,
+//       message: `Cannot update Group`,
+//     });
+//   }
+// };
--- routes/groups.js
+++ routes/groups.js
@@ -3,13 +3,19 @@ const {
   getAllGroups,
   getSingleGroup,
   createGroup,
-  updateGroup,
+  joinGroup,
+  leaveGroup,
+  addMessageGroup,
 } = require("../controllers/groups");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/").get(protect, getAllGroups).post(protect, createGroup);
 
-router.route("/:id").get(protect, getSingleGroup).patch(protect, updateGroup);
+router.route("/:id").get(protect, getSingleGroup);
+
+router.route("/join").post(protect, joinGroup);
+router.route("/leave").post(protect, leaveGroup);
+router.route("/message").post(protect, addMessageGroup);
 
 module.exports = router;
